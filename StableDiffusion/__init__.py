055e8d362aa0fa15e67c8663a0feba6d7389d997
Jaclyn Gu <uranium_238@163.com>
2023-07-02 08:29:38 -0700

    Will push code again after cleaning.

--- StableDiffusion/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-def GetStableDiffusionRootPath():
-    return os.path.abspath(os.path.dirname(__file__));
-
-def GetStableDiffusionSubpath(path):
-    return os.path.join(GetStableDiffusionRootPath(), path);
